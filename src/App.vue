44bd5167652753aae3bea790781bc1447c1d51b9
your_username <your_email@gmail.com>
2023-11-14 14:05:55 +0100

    Styling, Basic Config

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,34 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="MatchMaker logo" src="./assets/logo.png">
+  <MatchIDInput></MatchIDInput>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MatchIDInput from './components/MatchIDInput.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MatchIDInput
   }
 }
 </script>
 
 <style>
+body {
+  background-color: #001f25;
+  text-align: center;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #fff;
+  padding-top: 60px;
+  height: 100%;
+  width: 50%;
+  margin: 0 auto;
 }
 </style>
---
+++ src/components/MatchIDInput.vue
@@ -0,0 +1,52 @@
+<template>
+    <form id="MatchIDForm" @submit="checkForm" action="" method="post">
+      <p>
+        <label for="MatchID">1. Bitte gib die ID des Matches ein</label>
+      </p>
+      <div class="input-group mb-3">
+        <input id="MatchID" type="number" name="Match ID" class="form-control" aria-describedby="basic-addon2">
+        <div class="input-group-append">
+          <button class="btn btn-outline-secondary" type="submit" value="Bestätigen">Bestätigen</button>
+        </div>
+      </div>
+    </form>
+</template>
+
+<script>
+export default {
+  name: 'MatchIDInput',
+  methods: {
+    checkForm: function () {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.input-group {
+  width: 300px;
+  margin: 0 auto;
+}
+.btn-outline-secondary:hover {
+  background-color: #007772;
+}
+
+/*Pre-generated*/
+
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
