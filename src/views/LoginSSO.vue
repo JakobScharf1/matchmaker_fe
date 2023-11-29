646430ae81b3f8b8d0a54a8a06e90a61b4897f20
Team Operations <wematch-teamops@outlook.de>
2023-11-29 16:02:50 +0100

    Login integriert

---
+++ src/views/LoginSSO.vue
@@ -0,0 +1,18 @@
+<script setup>
+import loginSSO from "@/components/loginSSO.vue";
+
+export default {
+  name: "Login",
+  components: {
+    loginSSO,
+  }
+}
+</script>
+
+<template>
+  <loginSSO></loginSSO>
+</template>
+
+<style scoped>
+
+</style>
