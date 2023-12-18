4b574db12278d700c0128439f92ce15ea01ec9d7
Team Operations <wematch-teamops@outlook.de>
2023-12-18 17:02:04 +0100

    Authorisation Versuch

--- src/services/BackendService.js
+++ src/services/BackendService.js
@@ -5,7 +5,9 @@ const BACKEND_BASE_URL = "http://localhost:8081"
 class BackendService{
     getMatch(matchId){
         console.log("Übermittelt match ID: " + matchId);
-        return axios.get(BACKEND_BASE_URL + "/match/" + matchId);
+        const requestURI = BACKEND_BASE_URL + "/match/" + matchId;
+        console.log("Übermittelt an: " + requestURI);
+        return axios.get(requestURI);
     }
 }
 
