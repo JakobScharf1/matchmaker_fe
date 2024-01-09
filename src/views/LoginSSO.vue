3e95ca523163d762f7d223e00c2a8b9837c102f6
Team Operations <wematch-teamops@outlook.de>
2024-01-09 13:52:51 +0100

    Responsive-Warnung eingefügt

--- src/views/LoginSSO.vue
+++ src/views/LoginSSO.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <div class="mobileDeviceError" v-if="isMobile"><p>Diese Website ist für die Verwendung auf Mobilgeräten nicht optimiert. Bitte verwende einen PC oder ein Tablet.</p></div>
     <button class="btn btn-primary" @click="login()">Login mit Google</button>
     <!--<div v-if="userDetails">
       <h2>Benutzerdaten</h2>
@@ -19,12 +20,32 @@ import {login} from "@/firebase-config";
 export default {
   name: "LoginSSO",
 
+  data() {
+    return {
+      isMobile: false,
+    };
+  },
+
   methods: {
 
     async login() {
       login();
     },
+
+    checkIfMobile() {
+      this.isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+    }
   },
+
+  beforeUnmount() {
+    window.removeEventListener("resize", this.checkIfMobile);
+  },
+
+  mounted() {
+    this.checkIfMobile();
+    window.addEventListener("resize", this.checkIfMobile);
+  }
+
 }
 </script>
 
@@ -36,5 +57,11 @@ export default {
 
   .btn-primary:hover {
     background-color: transparent;
+  }
+
+  .mobileDeviceError {
+    color: red;
+    font-size: 1.5em;
+    text-align: center;
   }
 </style>
