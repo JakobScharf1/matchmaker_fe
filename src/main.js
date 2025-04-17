8029484a703595522ba616b9aa484e7d833a7800
SaemiDLastal <104317393+SaemiDLastal@users.noreply.github.com>
2025-04-17 14:25:23 +0200

    Fixing Firebase Config

--- src/main.js
+++ src/main.js
@@ -10,12 +10,12 @@ import firebase from "firebase/compat/app";
 
 // Firebase Config
 const firebaseConfig = {
-    apiKey: process.env.VUE_APP_FB_API_KEY,
-    authDomain: process.env.VUE_APP_FB_AUTH_DOMAIN,
-    projectId: process.env.VUE_APP_FB_PROJECT_ID,
-    storageBucket: process.env.VUE_APP_FB_STORAGE_BUCKET,
-    messagingSenderId: process.env.VUE_APP_FB_SENDER_ID,
-    appId: process.env.VUE_APP_FB_APP_ID
+    apiKey: "AIzaSyBsRgoDuNpx8iP8g50Ix_8EP84007bioU4",
+    authDomain: "signature-generator-e12e1.firebaseapp.com",
+    projectId: "signature-generator-e12e1",
+    storageBucket: "signature-generator-e12e1.firebasestorage.app",
+    messagingSenderId: "779310023193",
+    appId: "1:779310023193:web:3b1cf064b6df0ae529a088"
 };
 
 // Initialize Firebase
